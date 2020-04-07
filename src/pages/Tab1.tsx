7b1908d22bc8e57bb57253b7b1620f39909eb460
don <kdj00001@naver.com>
2020-04-07 23:32:05 +0900

    doc: README

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,23 +1,54 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent
+} from "@ionic/react";
+import React, { useState } from "react";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
+  // react-hook
+
+  const [content, setContent] = useState("");
+  const [list, setList] = useState(["123123", "23252"]);
+  // [SDFsdfs]
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>To-do-list</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        {content}
+        {list.map((v, i) => (
+          <IonCard>
+            <IonCardHeader>
+              <IonCardSubtitle>{v}</IonCardSubtitle>
+              <IonCardTitle>Card Title</IonCardTitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+              Keep close to Nature's heart... and break clear away, once in
+              awhile, and climb a mountain or spend a week in the woods. Wash
+              your spirit clean.
+            </IonCardContent>
+          </IonCard>
+        ))}
+        setContent 22123sdfsdfsdfsdfsdfsdfdsf dsfsdf sdf
+        <IonInput
+          placeholder="Enter Input"
+          onIonChange={e => setContent(e.detail.value!)}
+        ></IonInput>
+        IonButton
       </IonContent>
     </IonPage>
   );
